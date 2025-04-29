524847f201c8cd9ea544f3b0d51955fbbceeb308
Dusan <dusan@me.com>
2025-04-29 13:46:07 +0200

    s3 bucket fix

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -4,8 +4,3 @@
 resource "aws_s3_bucket" "entsoe_data" {
   bucket = var.s3_bucket_name
 }
-
-# Create an S3 bucket for Terraform backend
-resource "aws_s3_bucket" "terraform_backend" {
-  bucket = "entsoe-backend-bucket" # Replace with your backend bucket name
-}
